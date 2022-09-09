41e05fef8766036b5448719e283e731a63d5fb4a
Pingvu <alka04006@utb.vaxjo.se>
2022-09-09 15:00:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift_2._1
+{
+    class Program
+{
+    static void Main(string[]args)
+    {
+        Console.WriteLine("Hur långt har Elin hoppat? ");
+        String x = Console.ReadLine();
+            Console.WriteLine("Hur långt har Alma hoppart? ");
+            String Y = Console.ReadLine();
+            Console.WriteLine("Élin hoppade " + x ); 
+    }
+}
+}
